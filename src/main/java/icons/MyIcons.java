fbf3f3f8aa7b2e918dd2d80b9f0c194b95336567
sunyuchao <672208496@qq.com>
2023-12-01 10:58:11 +0800

    add stocking.png by preview issue

--- src/main/java/icons/MyIcons.java
+++ src/main/java/icons/MyIcons.java
@@ -6,5 +6,9 @@ import javax.swing.*;
 @SuppressWarnings("unused")
 public interface MyIcons {
 
+    Icon logoPng = IconLoader.getIcon("/icons/16x16/logo.png", MyIcons.class);
+
     Icon logo = IconLoader.getIcon("/icons/16x16/logo.svg", MyIcons.class);
+
+    Icon stocking = IconLoader.getIcon("/icons/16x16/stocking.png", MyIcons.class);
 }
